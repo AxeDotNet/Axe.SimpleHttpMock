11796c955813a76d2325ef82a458e6abf3e551e1
Liu Xia <lxconan@gmail.com>
2017-02-06 15:57:05 +0800

    Add configuration parameter when packaging.

--- build/pack-nuget-package.ps1
+++ build/pack-nuget-package.ps1
@@ -1,28 +1,37 @@
-
+param (
+    [string]$Configuration = "Release"
+)
 
 $ProjectRoot = "$PSScriptRoot/project"
 $LibraryDirectory = "$ProjectRoot/Axe.SimpleHttpMock"
 $TestDirectory = "$ProjectRoot/Axe.SimpleHttpMock.Test"
 
 function build_solution() {
-    Write-Host "Start building solution..." -ForegroundColor Green
+    Write-Host "Start building solution with following informations" -ForegroundColor Green
+    Write-Host "==================================================="
+    Write-Host "Configuration       $Configuration"
+    Write-Host "ProjectRoot         $ProjectRoot"
+    Write-Host "LibraryDirectory    $LibraryDirectory"
+    Write-Host "TestDirectory       $TestDirectory"
+    Write-Host "==================================================="
+
     Write-Host "Restoring package dependencies..." -ForegroundColor Green
     Invoke-Expression "dotnet restore '$ProjectRoot'"
     Write-Host "Restoring complete" -ForegroundColor Green
 
     Write-Host "Build projects..." -ForegroundColor Green
-    Invoke-Expression "dotnet build --configuration Release '$LibraryDirectory'"
-    Invoke-Expression "dotnet build --configuration Release '$TestDirectory'"
+    Invoke-Expression "dotnet build --configuration $Configuration '$LibraryDirectory'"
+    Invoke-Expression "dotnet build --configuration $Configuration '$TestDirectory'"
     Write-Host "Build complete" -ForegroundColor Green
 
     Write-Host "Running test for all targeting frameworks..." -ForegroundColor Green
-    Invoke-Expression "dotnet test --configuration Release '$TestDirectory'"
+    Invoke-Expression "dotnet test --configuration $Configuration '$TestDirectory'"
     Write-Host "Build succeeded"
 }
 
 function build_nuget_package() {
     Write-Host "Start packaging..." -ForegroundColor Green
-    Invoke-Expression "dotnet pack --configuration Release '$LibraryDirectory'"
+    Invoke-Expression "dotnet pack --configuration $Configuration '$LibraryDirectory'"
     Write-Host "Package complete" -ForegroundColor Green
 }
 
