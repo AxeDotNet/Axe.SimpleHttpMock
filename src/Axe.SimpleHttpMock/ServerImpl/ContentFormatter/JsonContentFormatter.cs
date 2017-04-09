0db34113a85dee14cddb7137416425661eceafd1
Liu Xia <lxconan@gmail.com>
2017-04-09 10:52:18 +0800

    Do not use default json serializer settings since it may be polluted.

--- src/Axe.SimpleHttpMock/ServerImpl/ContentFormatter/JsonContentFormatter.cs
+++ src/Axe.SimpleHttpMock/ServerImpl/ContentFormatter/JsonContentFormatter.cs
@@ -3,12 +3,21 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Axe.SimpleHttpMock.ServerImpl.ContentFormatter
 {
     class JsonContentFormatter : IContentSerializer, IContentDeserializer
     {
         const string MediaType = "application/json";
+
+        static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.None,
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            DateFormatHandling = DateFormatHandling.IsoDateFormat
+        };
+
         readonly Encoding encoding;
         
         public JsonContentFormatter(Encoding encoding = null)
@@ -18,25 +27,25 @@ namespace Axe.SimpleHttpMock.ServerImpl.ContentFormatter
         
         public HttpContent Format(object payload)
         {
-            string serialized = JsonConvert.SerializeObject(payload);
+            string serialized = JsonConvert.SerializeObject(payload, JsonSerializerSettings);
             return new StringContent(serialized, encoding, MediaType);
         }
 
         public Task<object> DeserializeAsync(HttpContent content)
         {
-            return DeserializeContentAsync(content, JsonConvert.DeserializeObject);
+            return DeserializeContentAsync(content, jsonStr => JsonConvert.DeserializeObject(jsonStr, JsonSerializerSettings));
         }
 
         public Task<T> DeserializeAsync<T>(HttpContent content)
         {
-            return DeserializeContentAsync(content, JsonConvert.DeserializeObject<T>);
+            return DeserializeContentAsync(content, jsonStr => JsonConvert.DeserializeObject<T>(jsonStr, JsonSerializerSettings));
         }
 
         public Task<T> DeserializeAsync<T>(HttpContent content, T template)
         {
             return DeserializeContentAsync(
                 content,
-                s => JsonConvert.DeserializeAnonymousType(s, template));
+                s => JsonConvert.DeserializeAnonymousType(s, template, JsonSerializerSettings));
         }
 
         static async Task<T> DeserializeContentAsync<T>(HttpContent content, Func<string, T> deserializingFunc)
