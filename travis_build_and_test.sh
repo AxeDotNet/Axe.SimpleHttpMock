49c4d6923bf87449495fe5b9f17a49109869019b
Liu Xia <lxconan@gmail.com>
2017-04-11 11:47:32 +0800

    Fix build script slash.

--- travis_build_and_test.sh
+++ travis_build_and_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dotnet restore
 dotnet build
-dotnet test ./test/Axe.SimpleHttpMock.Test\Axe.SimpleHttpMock.Test.csproj
+dotnet test ./test/Axe.SimpleHttpMock.Test/Axe.SimpleHttpMock.Test.csproj
